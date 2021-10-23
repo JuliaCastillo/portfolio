60eb8b5a891ac1d3507836af47580c03c624da99
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-10-23 18:13:52 +0200

    started on homepage

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,18 @@
 import React from "react";
+import stroke from "../assets/img/brush-stroke-cropped.svg";
 
 function Hero() {
   return (
-    <section id="">
-      <div className="wrapper"></div>
+    <section className="hero" id="hero">
+      <div className="section_wrapper hero__content">
+        <img
+          src={stroke}
+          className="hero__image"
+          alt="horizontal brush stroke"
+        />
+        <h1>HI, I'M JULIA</h1>
+        <p>productivity nut, animal lover, front end developer</p>
+      </div>
     </section>
   );
 }
