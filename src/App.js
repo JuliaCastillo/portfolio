94f8f7dbce638f1a98691469144a8aed848c791a
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-10-23 17:16:43 +0200

    Added main components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from "./assets/img/logo.svg";
+import React from "react";
 import "./assets/css/styles.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactive
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </main>
   );
 }
 
