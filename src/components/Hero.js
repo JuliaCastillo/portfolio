69c514d5c44608a01cde110503909b7838ffde08
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-10-25 13:42:35 +0200

    added contact logos

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,29 @@
 import React from "react";
-// import stroke from "../assets/img/brush-stroke-cropped.svg";
+import githubLogo from "../assets/img/github.svg";
+import linkedinLogo from "../assets/img/linkedin.svg";
+import emailLogo from "../assets/img/email.svg";
 
 function Hero() {
   return (
     <section className="hero" id="hero">
       <div className="section_wrapper hero__content">
-        {/* <img
-          src={stroke}
-          className="hero__image"
-          alt="horizontal brush stroke"
-        /> */}
         <h1>HI, I'M JULIA</h1>
         <p>
           <span>productivity nut, </span> <span>animal lover, </span>
           <span>front end developer</span>
         </p>
+
+        <div className="hero__links">
+          <a href="https://github.com/JuliaCastillo">
+            <img src={githubLogo} alt="GitHub logo" />
+          </a>
+          <a href="https://www.linkedin.com/in/julia-castillo-trujillo-1bb375172/">
+            <img src={linkedinLogo} alt="LinkedIn logo" />
+          </a>
+          <a href="mailto:juliacastillotrujillo@gmail.com">
+            <img src={emailLogo} alt="Email logo" />
+          </a>
+        </div>
       </div>
     </section>
   );
