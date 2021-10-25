710471ef7b8e2ccd46018011b21cf452856c8f23
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-10-25 13:09:23 +0200

    title and subtitle of hero done for mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,20 @@
 import React from "react";
-import stroke from "../assets/img/brush-stroke-cropped.svg";
+// import stroke from "../assets/img/brush-stroke-cropped.svg";
 
 function Hero() {
   return (
     <section className="hero" id="hero">
       <div className="section_wrapper hero__content">
-        <img
+        {/* <img
           src={stroke}
           className="hero__image"
           alt="horizontal brush stroke"
-        />
+        /> */}
         <h1>HI, I'M JULIA</h1>
-        <p>productivity nut, animal lover, front end developer</p>
+        <p>
+          <span>productivity nut, </span> <span>animal lover, </span>
+          <span>front end developer</span>
+        </p>
       </div>
     </section>
   );
