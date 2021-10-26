961bd962f73fbc1cc236c0df1c9bfbf7904ada27
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-10-26 12:57:45 +0200

    card button for mobile done

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,12 +5,14 @@ function ProjectCard({ title, description, codeLink, demoLink, image }) {
     <div className="project-card">
       <h3>{title}</h3>
       <p>{description}</p>
-      <a href={codeLink} className="button">
-        Code
-      </a>
-      <a href={demoLink} className="button">
-        Live website
-      </a>
+      <div className="card-buttons">
+        <a href={codeLink} className="button">
+          Code
+        </a>
+        <a href={demoLink} className="button">
+          Live website
+        </a>
+      </div>
     </div>
   );
 }
