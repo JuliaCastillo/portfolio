7b6031dbbd4821bb20d65cb2de6055e0a3d2c6da
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-10-26 12:09:00 +0200

    added section title to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,14 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
 
 function Projects() {
   return (
-    <section id="projects">
-      <div className="wrapper"></div>
+    <section id="projects" className="projects">
+      <div className="section_wrapper">
+        <div className="section_title">
+          <h2>PROJECTS</h2>
+        </div>
+      </div>
     </section>
   );
 }
