f619f6c6077de3659e2023807ddaf77a1a0ca058
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-10-27 11:37:07 +0200

    started nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,21 @@ import React from "react";
 
 function Navbar() {
   return (
-    <section id="">
-      <div className="wrapper"></div>
-    </section>
+    <header id="header" className="header">
+      <div className="header__wrapper">
+        <a href="#" class="header__toggle" id="btnHamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
+
+        <nav className="header__nav">
+          <a href="#about">About me</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </div>
+    </header>
   );
 }
 
