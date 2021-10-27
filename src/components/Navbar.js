deb17fd2ba0f300e9648638126d5f8303e5d3717
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-10-27 12:23:53 +0200

    hamburger icon done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 function Navbar() {
-  return (
-    <header id="header" className="header">
-      <div className="header__wrapper">
-        <a href="#" class="header__toggle" id="btnHamburger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </a>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-        <nav className="header__nav">
-          <a href="#about">About me</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
-        </nav>
-      </div>
+  return (
+    <header id="header" className={menuOpen ? "header active" : "header"}>
+      {/* <div className="header__wrapper"> */}
+      <a
+        href="#"
+        className="header__toggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+
+      <nav className="header__nav" onClick={() => setMenuOpen(false)}>
+        <a href="#about">About me</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+      {/* </div> */}
     </header>
   );
 }
