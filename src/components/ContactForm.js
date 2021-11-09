132b7237f9011d47faef0e27c0ece2d1ea18cdee
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-11-09 17:17:56 +0100

    changed to controlled inputs in form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,8 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "emailjs-com";
 
 function ContactForm({ setSnackbarOpen }) {
   const form = useRef();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -17,6 +21,9 @@ function ContactForm({ setSnackbarOpen }) {
       .then(
         (result) => {
           setSnackbarOpen(true);
+          setName("");
+          setEmail("");
+          setMessage("");
           console.log(result.text);
         },
         (error) => {
@@ -29,7 +36,14 @@ function ContactForm({ setSnackbarOpen }) {
     <form ref={form} onSubmit={sendEmail}>
       <div className="input-wrapper name-input">
         <label for="name-input">Name</label>
-        <input id="name-input" type="text" name="name" placeholder="Jane Doe" />
+        <input
+          id="name-input"
+          type="text"
+          name="name"
+          placeholder="Jane Doe"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div className="input-wrapper email-input">
         <label for="email-input">Email</label>
@@ -38,12 +52,20 @@ function ContactForm({ setSnackbarOpen }) {
           type="email"
           name="email"
           placeholder="janedoe123@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           required
         />
       </div>
       <div className="input-wrapper message-input">
         <label for="message-input">Message</label>
-        <textarea id="message-input" name="message" rows="6" />
+        <textarea
+          id="message-input"
+          name="message"
+          rows="6"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
       </div>
       <input type="submit" value="Send" />
     </form>
